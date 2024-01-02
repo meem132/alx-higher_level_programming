57e16dec25c828a44578f2f7b5cc76dfbeec2c3c
meem132 <memesmeem132@gmail.com>
2024-01-02 12:34:17 -0800

    task 3+

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for a in range(ord("a"), ord("z") + 1):
-    if(chr(a) is not "q" and chr(a) is not "e"):
-        print("{:s}".format(chr(a)), end="")
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
